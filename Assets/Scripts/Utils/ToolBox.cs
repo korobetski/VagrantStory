94c341c780da7e83bb46641d89bc3a7865aff874
Korobetski Sigfrid <lunaticchimera@gmail.com>
2019-12-28 15:03:02 +0100

    Swiss Army Knife :fork_and_knife:

---
+++ Assets/Scripts/Utils/ToolBox.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+namespace Utils
+{
+    public class ToolBox
+    {
+        public static uint GetNumPositiveBits(int bnumt)
+        {
+            uint inc = 0;
+            BitArray b = new BitArray(new int[] { bnumt });
+            if (bnumt != 0)
+            {
+                for (var j = 0; j < b.Length; j++)
+                {
+                    if (b.Get(j))
+                    {
+                        inc++;
+                    }
+                }
+            }
+            return inc;
+        }
+        public static string GetGameObjectPath(GameObject obj, string baseName)
+        {
+            string path = obj.name;
+            while (obj.transform.parent != null)
+            {
+                obj = obj.transform.parent.gameObject;
+                path = obj.name + "/" + path;
+            }
+            path = path.Remove(0, baseName.Length + 1);
+            return path;
+        }
+        public static GameObject findBoneIn(string boneName, GameObject obj)
+        {
+            foreach (Transform child in obj.transform)
+            {
+                if (child.name == boneName)
+                {
+                    return child.gameObject;
+                }
+                else
+                {
+                    GameObject littleChild = findBoneIn(boneName, child.gameObject);
+                    if (littleChild != null)
+                    {
+                        return littleChild;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        internal static Color[] ExpandColors(Color[] colors, ushort? count = null)
+        {
+            Color[] newColors = new Color[colors.Length * 2];
+            for (var i = 0; i < colors.Length; i++)
+            {
+                newColors[i * 2] = colors[i];
+                if (i < colors.Length - 1)
+                {
+                    newColors[i * 2 + 1] = Color.Lerp(colors[i], colors[i + 1], 0.5f);
+                }
+                else
+                {
+                    newColors[i * 2 + 1] = Color.Lerp(colors[i], Color.black, 0.5f);
+                }
+            }
+
+            if (count != null)
+            {
+                if (newColors.Length < count)
+                {
+                    newColors = ExpandColors(newColors, count);
+                }
+            }
+
+            return newColors;
+        }
+
+        public static Quaternion quatFromAxisAnle(Vector3 axis, float angle)
+        {
+            Quaternion q = new Quaternion(axis.x * Mathf.Sin(angle / 2), axis.y * Mathf.Sin(angle / 2), axis.z * Mathf.Sin(angle / 2), Mathf.Cos(angle / 2));
+            q.Normalize();
+            return q;
+        }
+        public static float rot13toRad(int angle)
+        {
+            float f = Mathf.PI / 4096;
+            return (float)(f * angle);
+        }
+        public static byte[] EndianSwitcher(byte[] bytes)
+        {
+            Array.Reverse(bytes);
+            return bytes;
+        }
+        public static void DirExNorCreate(string dirPath)
+        {
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+        }
+        public static Color32 BitColorConverter(ushort rawColor)
+        {
+            if (rawColor == 0)
+            {
+                return new Color32(0, 0, 0, 0);
+            }
+            else
+            {
+                int a = (rawColor & 0x8000) >> 15;
+                int b = (rawColor & 0x7C00) >> 10;
+                int g = (rawColor & 0x03E0) >> 5;
+                int r = (rawColor & 0x001F);
+                if (r == 0 && g == 0 && b == 0)
+                {
+                    if ((rawColor & 0x8000) == 0)
+                    {
+                        // black, and the alpha bit is NOT set
+                        a = (byte)0; // totally transparent
+                    }
+                    else
+                    {
+                        // black, and the alpha bit IS set
+                        a = (byte)255; // totally opaque
+                    }
+                }
+                else
+                {
+                    if ((rawColor & 0x8000) == 0)
+                    {
+                        // some color, and the alpha bit is NOT set
+                        a = (byte)255; // totally opaque
+                    }
+                    else
+                    {
+                        // some color, and the alpha bit IS set
+                        a = (byte)250; // some variance of transparency
+                    }
+                }
+
+                Color32 color = new Color32((byte)(r * 8), (byte)(g * 8), (byte)(b * 8), (byte)a);
+                return color;
+            }
+        }
+
+
+        internal static void DestroyChildren(GameObject gameObject, bool coroutine = false)
+        {
+            int cc = gameObject.transform.childCount;
+            if (cc > 0)
+            {
+                foreach (Transform child in gameObject.transform)
+                {
+#if UNITY_EDITOR
+                    if (coroutine)
+                    {
+                        UnityEditor.EditorApplication.delayCall += () =>
+                        {
+                            GameObject.DestroyImmediate(child.gameObject);
+                        };
+                    }
+                    else
+                    {
+                        GameObject.DestroyImmediate(child.gameObject);
+                    }
+#else
+                    GameObject.Destroy(child.gameObject);
+#endif
+
+                }
+
+            }
+        }
+
+        public static void WriteJSON(string path, string data)
+        {
+            File.WriteAllText(path, data);
+        }
+    }
+}
---
+++ Assets/Scripts/Vagrant Story/Component/CouteauSuisse.cs
@@ -0,0 +1,335 @@
+﻿using MyBox;
+using System;
+using UnityEngine;
+using VagrantStory.Core;
+using VagrantStory.Database;
+using Utils;
+
+namespace VagrantStory.Component
+{
+
+    public class CouteauSuisse : MonoBehaviour
+    {
+
+        public MaterialsDB.eMaterials material = MaterialsDB.eMaterials.Bronze;
+        [Separator("Blade")]
+        public BladesDB.eBladeCategory category = BladesDB.eBladeCategory.Sword;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Dagger)] public BladesDB.eDaggerBlades dagger = BladesDB.eDaggerBlades.None;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Sword)] public BladesDB.eSwordBlades sword = BladesDB.eSwordBlades.None;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Great_Sword)] public BladesDB.eGreatSwordBlades greatSword = BladesDB.eGreatSwordBlades.None;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Axe)] public BladesDB.eAxeBlades axe = BladesDB.eAxeBlades.None;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Mace)] public BladesDB.eMaceBlades mace = BladesDB.eMaceBlades.None;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Great_Axe)] public BladesDB.eGreatAxeBlades greatAxe = BladesDB.eGreatAxeBlades.None;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Staff)] public BladesDB.eStaffBlades staff = BladesDB.eStaffBlades.None;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Heavy_Mace)] public BladesDB.eHeavyMaceBlades heavyMace = BladesDB.eHeavyMaceBlades.None;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Polearm)] public BladesDB.ePolearmBlades polearm = BladesDB.ePolearmBlades.None;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Crossbow)] public BladesDB.eCrossbowBlades crossbow = BladesDB.eCrossbowBlades.None;
+        [ConditionalField(nameof(category), false, BladesDB.eBladeCategory.Shield)] public ArmorsDB.eShields shield = ArmorsDB.eShields.None;
+
+        [Separator("Grip")]
+        [ReadOnly] public GripsDB.eGripCategories gripType = GripsDB.eGripCategories.Guard;
+        [ConditionalField(nameof(gripType), false, GripsDB.eGripCategories.Guard)] public GripsDB.eGuards guard = GripsDB.eGuards.None;
+        [ConditionalField(nameof(gripType), false, GripsDB.eGripCategories.Grip)] public GripsDB.eGrips grip = GripsDB.eGrips.None;
+        [ConditionalField(nameof(gripType), false, GripsDB.eGripCategories.Pole)] public GripsDB.ePoles pole = GripsDB.ePoles.None;
+        [ConditionalField(nameof(gripType), false, GripsDB.eGripCategories.Bolt)] public GripsDB.eBolts bolt = GripsDB.eBolts.None;
+
+        [Separator("Gems")]
+        [ReadOnly] public uint gemSlot = 0;
+        [HideInInspector] public bool gemSlot1 = false;
+        [HideInInspector] public bool gemSlot2 = false;
+        [HideInInspector] public bool gemSlot3 = false;
+        [ConditionalField(nameof(gemSlot1))] public GemsDB.eGems gem1 = GemsDB.eGems.None;
+        [ConditionalField(nameof(gemSlot2))] public GemsDB.eGems gem2 = GemsDB.eGems.None;
+        [ConditionalField(nameof(gemSlot3))] public GemsDB.eGems gem3 = GemsDB.eGems.None;
+
+
+        [Separator("Statistics")]
+        [ReadOnly] public Statistics statistics;
+
+
+
+        private byte _modelId = 0;
+        private int _material = 0;
+        private GameObject _wepPrefab;
+
+        private void Start()
+        {
+        }
+
+
+        private void OnValidate()
+        {
+            bool reloadModel = false;
+
+            statistics = new Statistics();
+            if (material != MaterialsDB.eMaterials.None)
+            {
+                statistics += MaterialsDB.List[(int)material].statistics;
+                if (_material != (int)material) reloadModel = true;
+                _material = (int)material;
+            }
+
+            if (category == BladesDB.eBladeCategory.Dagger && dagger != BladesDB.eDaggerBlades.None)
+            {
+                statistics += BladesDB.List[(int)dagger].statistics;
+                if (_modelId != BladesDB.List[(int)dagger].wepID)
+                {
+                    _modelId = BladesDB.List[(int)dagger].wepID;
+                    reloadModel = true;
+                }
+            }
+            else if (category == BladesDB.eBladeCategory.Sword && sword != BladesDB.eSwordBlades.None)
+            {
+                statistics += BladesDB.List[(int)sword].statistics;
+                if (_modelId != BladesDB.List[(int)sword].wepID)
+                {
+                    _modelId = BladesDB.List[(int)sword].wepID;
+                    reloadModel = true;
+                }
+            }
+            else if (category == BladesDB.eBladeCategory.Great_Sword && greatSword != BladesDB.eGreatSwordBlades.None)
+            {
+                statistics += BladesDB.List[(int)greatSword].statistics;
+                if (_modelId != BladesDB.List[(int)greatSword].wepID)
+                {
+                    _modelId = BladesDB.List[(int)greatSword].wepID;
+                    reloadModel = true;
+                }
+            }
+            else if (category == BladesDB.eBladeCategory.Axe && axe != BladesDB.eAxeBlades.None)
+            {
+                statistics += BladesDB.List[(int)axe].statistics;
+                if (_modelId != BladesDB.List[(int)axe].wepID)
+                {
+                    _modelId = BladesDB.List[(int)axe].wepID;
+                    reloadModel = true;
+                }
+            }
+            else if (category == BladesDB.eBladeCategory.Mace && mace != BladesDB.eMaceBlades.None)
+            {
+                statistics += BladesDB.List[(int)mace].statistics;
+                if (_modelId != BladesDB.List[(int)mace].wepID)
+                {
+                    _modelId = BladesDB.List[(int)mace].wepID;
+                    reloadModel = true;
+                }
+            }
+            else if (category == BladesDB.eBladeCategory.Great_Axe && greatAxe != BladesDB.eGreatAxeBlades.None)
+            {
+                statistics += BladesDB.List[(int)greatAxe].statistics;
+                if (_modelId != BladesDB.List[(int)greatAxe].wepID)
+                {
+                    _modelId = BladesDB.List[(int)greatAxe].wepID;
+                    reloadModel = true;
+                }
+            }
+            else if (category == BladesDB.eBladeCategory.Staff && staff != BladesDB.eStaffBlades.None)
+            {
+                statistics += BladesDB.List[(int)staff].statistics;
+                if (_modelId != BladesDB.List[(int)staff].wepID)
+                {
+                    _modelId = BladesDB.List[(int)staff].wepID;
+                    reloadModel = true;
+                }
+            }
+            else if (category == BladesDB.eBladeCategory.Heavy_Mace && heavyMace != BladesDB.eHeavyMaceBlades.None)
+            {
+                statistics += BladesDB.List[(int)heavyMace].statistics;
+                if (_modelId != BladesDB.List[(int)heavyMace].wepID)
+                {
+                    _modelId = BladesDB.List[(int)heavyMace].wepID;
+                    reloadModel = true;
+                }
+            }
+            else if (category == BladesDB.eBladeCategory.Polearm && polearm != BladesDB.ePolearmBlades.None)
+            {
+                statistics += BladesDB.List[(int)polearm].statistics;
+                if (_modelId != BladesDB.List[(int)polearm].wepID)
+                {
+                    _modelId = BladesDB.List[(int)polearm].wepID;
+                    reloadModel = true;
+                }
+            }
+            else if (category == BladesDB.eBladeCategory.Crossbow && crossbow != BladesDB.eCrossbowBlades.None)
+            {
+                statistics += BladesDB.List[(int)crossbow].statistics;
+                if (_modelId != BladesDB.List[(int)crossbow].wepID)
+                {
+                    _modelId = BladesDB.List[(int)crossbow].wepID;
+                    reloadModel = true;
+                }
+            }
+            else if (category == BladesDB.eBladeCategory.Shield && shield != ArmorsDB.eShields.None)
+            {
+                statistics += ArmorsDB.ShieldList[(int)shield].statistics;
+                if (_modelId != ArmorsDB.ShieldList[(int)shield].wepID)
+                {
+                    _modelId = ArmorsDB.ShieldList[(int)shield].wepID;
+                    reloadModel = true;
+                }
+            }
+
+            if (category == BladesDB.eBladeCategory.Dagger || category == BladesDB.eBladeCategory.Sword || category == BladesDB.eBladeCategory.Great_Sword)
+            {
+                gripType = GripsDB.eGripCategories.Guard;
+                grip = GripsDB.eGrips.None;
+                pole = GripsDB.ePoles.None;
+                bolt = GripsDB.eBolts.None;
+
+                if (guard != GripsDB.eGuards.None)
+                {
+                    gemSlot = GripsDB.List[(int)guard].gemSlots;
+                    statistics += GripsDB.List[(int)guard].statistics;
+                }
+                else
+                {
+                    gemSlot = 0;
+                }
+
+                RefreshGemSlots();
+            }
+            else if (category == BladesDB.eBladeCategory.Axe || category == BladesDB.eBladeCategory.Mace || category == BladesDB.eBladeCategory.Great_Axe 
+                || category == BladesDB.eBladeCategory.Staff || category == BladesDB.eBladeCategory.Heavy_Mace)
+            {
+                gripType = GripsDB.eGripCategories.Grip;
+                guard = GripsDB.eGuards.None;
+                pole = GripsDB.ePoles.None;
+                bolt = GripsDB.eBolts.None;
+                if (grip != GripsDB.eGrips.None)
+                {
+                    gemSlot = GripsDB.List[(int)grip].gemSlots; statistics += GripsDB.List[(int)grip].statistics;
+                }
+                else
+                {
+                    gemSlot = 0;
+                }
+
+                RefreshGemSlots();
+            }
+            else if (category == BladesDB.eBladeCategory.Polearm)
+            {
+                gripType = GripsDB.eGripCategories.Pole;
+                guard = GripsDB.eGuards.None;
+                grip = GripsDB.eGrips.None;
+                bolt = GripsDB.eBolts.None;
+                if (pole != GripsDB.ePoles.None) { gemSlot = GripsDB.List[(int)pole].gemSlots; statistics += GripsDB.List[(int)pole].statistics; }
+                else
+                {
+                    gemSlot = 0;
+                }
+
+                RefreshGemSlots();
+            }
+            else if (category == BladesDB.eBladeCategory.Crossbow)
+            {
+                gripType = GripsDB.eGripCategories.Bolt;
+                guard = GripsDB.eGuards.None;
+                grip = GripsDB.eGrips.None;
+                pole = GripsDB.ePoles.None;
+                if (bolt != GripsDB.eBolts.None) { gemSlot = GripsDB.List[(int)bolt].gemSlots; statistics += GripsDB.List[(int)bolt].statistics; }
+                else
+                {
+                    gemSlot = 0;
+                }
+
+                RefreshGemSlots();
+            }
+            else if (category == BladesDB.eBladeCategory.Shield)
+            {
+                // shields have no grip, but have gem slots
+                gripType = GripsDB.eGripCategories.None;
+                guard = GripsDB.eGuards.None;
+                grip = GripsDB.eGrips.None;
+                pole = GripsDB.ePoles.None;
+                bolt = GripsDB.eBolts.None;
+
+                if (shield != ArmorsDB.eShields.None)
+                {
+                    gemSlot = ArmorsDB.ShieldList[(int)shield].gemSlots;
+                }
+                else
+                {
+                    gemSlot = 0;
+                }
+
+                RefreshGemSlots();
+            }
+            else
+            {
+                gripType = GripsDB.eGripCategories.None;
+                guard = GripsDB.eGuards.None;
+                grip = GripsDB.eGrips.None;
+                pole = GripsDB.ePoles.None;
+                bolt = GripsDB.eBolts.None;
+                gemSlot = 0;
+                RefreshGemSlots();
+            }
+
+            if (reloadModel)
+            {
+                LoadWEPModel(_modelId);
+            }
+        }
+
+        private void LoadWEPModel(byte wepId)
+        {
+            ToolBox.DestroyChildren(gameObject, true);
+
+            string wepPath = string.Concat("Prefabs/Weapons/", BitConverter.ToString(new byte[] { wepId }));
+
+
+            _wepPrefab = Instantiate(Resources.Load(wepPath)) as GameObject;
+            _wepPrefab.transform.parent = gameObject.transform;
+
+            Vector2[] offsets = new Vector2[8] { Vector2.zero, Vector2.zero, new Vector2(0, 0.25f), new Vector2(0, 0.5f), new Vector2(0, 0.75f),
+                new Vector2(0.5f, 0.25f), new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.75f) };
+            // need to check ingame textures
+
+            _wepPrefab.GetComponent<MeshRenderer>().sharedMaterial.SetTextureOffset("_MainTex", offsets[(int)material]);
+        }
+
+        private void RefreshGemSlots()
+        {
+            gemSlot1 = (gemSlot > 0);
+            gemSlot2 = (gemSlot > 1);
+            gemSlot3 = (gemSlot > 2);
+
+            if (!gemSlot1)
+            {
+                gem1 = GemsDB.eGems.None;
+            }
+            else
+            {
+                if (gem1 != GemsDB.eGems.None)
+                {
+                    statistics += GemsDB.List[(int)gem1].statistics;
+                }
+            }
+
+            if (!gemSlot2)
+            {
+                gem2 = GemsDB.eGems.None;
+            }
+            else
+            {
+                if (gem2 != GemsDB.eGems.None)
+                {
+                    statistics += GemsDB.List[(int)gem2].statistics;
+                }
+            }
+
+            if (!gemSlot3)
+            {
+                gem3 = GemsDB.eGems.None;
+            }
+            else
+            {
+                if (gem3 != GemsDB.eGems.None)
+                {
+                    statistics += GemsDB.List[(int)gem3].statistics;
+                }
+            }
+        }
+    }
+
+}
