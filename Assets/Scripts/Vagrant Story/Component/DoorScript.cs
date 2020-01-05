0ef420983aea5aa7ffceb6db0a2cdebafafad763
Korobetski Sigfrid <lunaticchimera@gmail.com>
2020-01-05 14:22:59 +0100

    A bit of Post Process :rainbow:

---
+++ Assets/Scripts/Vagrant Story/Component/DoorScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace VagrantStory.Component
+{
+
+    public class DoorScript : MonoBehaviour
+    {
+        public bool openned = false;
+        public bool inverse = false;
+        public bool locked = false;
+        public byte lockId = 0;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (openned)
+            {
+                if (inverse)
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+                } else
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnValidate()
+        {
+
+            if (openned)
+            {
+                if (inverse)
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+                }
+            } else
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
+        }
+    }
+
+}
